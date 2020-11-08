c0b5ac5fd72a8d4256686fc4685a1f9a63010148
ekardnam <ekarndam@autistici.org>
2020-11-08 16:29:01 +0000

    Make it only visualize the image

--- scripts/visualize_image.py
+++ scripts/visualize_image.py
@@ -38,34 +38,6 @@ if __name__ == '__main__':
     image = tf.image.resize(image, (480, 640))
     image_batch = tf.expand_dims(image, axis=0)
 
-    # use this configuration configuration
-    CONFIGURATION_PROTO = {
-
-        'image_width': 640,
-        'image_height': 480,
-
-        'base_model_stride': 16,
-
-        'anchor_scales': [32, 64, 128],
-        'anchor_ratios': [1],
-
-        'batch_size': 1,
-        'region_count': 32,
-
-        'high_threshold': 0.7,
-        'low_threshold': 0.3
-
-    }
-    config = get_configuration(config_proto=CONFIGURATION_PROTO)
-
-    anchors = generate_anchors(config)
-    gt_boxes = tf.constant([image_annotation['objects']])
-    gt_boxes = normalize_bboxes(gt_boxes, image_annotation['image_height'], image_annotation['image_width'])
-
-    labels, deltas = get_target(anchors, gt_boxes, config)
-    predicted_anchors = get_bounding_boxes_from_labels(labels, config)
-    predicted_boxes = apply_deltas_to_bounding_boxes(predicted_anchors, tf.reshape(deltas, [1, -1, 4]))
-    image_batch = draw_bounding_boxes(image_batch, predicted_boxes)
     writer = tf.summary.create_file_writer(args.out)
     with writer.as_default():
         draw_image_batch('Image', image_batch)
