1d481020441669c4409ee19a16bd4add643722d4
ekardnam <ekarndam@autistici.org>
2020-11-21 18:12:32 +0000

    Change regularization

--- head_rpn/model.py
+++ head_rpn/model.py
@@ -31,6 +31,6 @@ def get_regularized_model(config):
     base_model = VGG16(include_top=False, input_shape=(image_height, image_width, 3))
     vgg16_feature_map = base_model.get_layer('block5_conv3').output
     output = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer='l2', name='RPN_conv')(vgg16_feature_map)
-    rpn_class = Conv2D(config['anchor_count'], (1, 1), activation='sigmoid', kernel_regularizer='l2', name='RPN_classifier')(output)
-    rpn_regr = Conv2D(config['anchor_count'] * 4, (1, 1), activation='linear', kernel_regularizer='l2', name='RPN_regressor')(output)
+    rpn_class = Conv2D(config['anchor_count'], (1, 1), activation='sigmoid', name='RPN_classifier')(output)
+    rpn_regr = Conv2D(config['anchor_count'] * 4, (1, 1), activation='linear', name='RPN_regressor')(output)
     return Model(inputs=base_model.input, outputs=[rpn_class, rpn_regr])
