dd0fed90a19a164232a694e30028f93e38a42984
MarcoBarducci12 <64955381+MarcoBarducci12@users.noreply.github.com>
2020-12-14 19:46:27 +0100

    update model for fine tuning
    Just added a second function that returns a model with non freezed block5 conv layers

--- head_rpn/model.py
+++ head_rpn/model.py
@@ -34,3 +34,28 @@ def get_regularized_model(config):
     rpn_class = Conv2D(config['anchor_count'], (1, 1), activation='sigmoid', name='RPN_classifier')(output)
     rpn_regr = Conv2D(config['anchor_count'] * 4, (1, 1), activation='linear', name='RPN_regressor')(output)
     return Model(inputs=base_model.input, outputs=[rpn_class, rpn_regr])
+def get_model_for_tuning(config):
+     """
+        Creates the RPN model from the config using L2 regularization
+        Args:
+            config,  the configuration dictionary
+        Returns:
+            the Keras RPN model
+    """
+    image_height, image_width = config['image_height'], config['image_width']
+    base_model = VGG16(include_top=False, input_shape=(image_height, image_width, 3))
+    base_model.trainable = True
+    set_trainable = False
+    for layer in base_model.layers:
+        if layer.name == 'block5_conv1' or layer.name == 'block5_conv2' or layer.name == 'block5_conv3':
+           set_trainable = True
+        if set_trainable:
+           layer.trainable = True
+        else:
+            layer.trainable = False
+    vgg16_feature_map = base_model.get_layer('block5_conv3').output
+    output = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer='l2', name='RPN_conv')(vgg16_feature_map)
+    rpn_class = Conv2D(config['anchor_count'], (1, 1), activation='sigmoid', name='RPN_classifier')(output)
+    rpn_regr = Conv2D(config['anchor_count'] * 4, (1, 1), activation='linear', name='RPN_regressor')(output)
+    return Model(inputs=base_model.input, outputs=[rpn_class, rpn_regr])
+    
