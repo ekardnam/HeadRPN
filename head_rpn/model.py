66844aee66360ba96ab289c749f0975195fde69a
MarcoBarducci12 <64955381+MarcoBarducci12@users.noreply.github.com>
2020-12-18 13:56:52 +0100

    update model.py

--- head_rpn/model.py
+++ head_rpn/model.py
@@ -5,6 +5,9 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.models import load_model
 from .train import classifier_loss, regressor_loss
 
+""" Dictionary that contains custom objects we have to pass to load_model
+    We need to specify them because they are not keras built-in objects """
+
 custom_objs = {'classifier_loss': classifier_loss, 
               'regressor_loss': regressor_loss }
 
@@ -18,6 +21,7 @@ def get_model(config):
     """
     image_height, image_width = config['image_height'], config['image_width']
     base_model = VGG16(include_top=False, input_shape=(image_height, image_width, 3))
+    base_model.trainable = False
     vgg16_feature_map = base_model.get_layer('block5_conv3').output
     output = Conv2D(512, (3, 3), activation='relu', padding='same', name='RPN_conv')(vgg16_feature_map)
     rpn_class = Conv2D(config['anchor_count'], (1, 1), activation='sigmoid', name='RPN_classifier')(output)
@@ -34,6 +38,7 @@ def get_regularized_model(config):
     """
     image_height, image_width = config['image_height'], config['image_width']
     base_model = VGG16(include_top=False, input_shape=(image_height, image_width, 3))
+    base_model.trainable = False
     vgg16_feature_map = base_model.get_layer('block5_conv3').output
     output = Conv2D(512, (3, 3), activation='relu', padding='same', kernel_regularizer='l2', name='RPN_conv')(vgg16_feature_map)
     rpn_class = Conv2D(config['anchor_count'], (1, 1), activation='sigmoid', name='RPN_classifier')(output)
@@ -41,6 +46,17 @@ def get_regularized_model(config):
     return Model(inputs=base_model.input, outputs=[rpn_class, rpn_regr])
 
 def get_model_for_tuning(name, custom_obj):
+  """
+        Loads the RPN model trained previously for fine_tuning
+        Here we let the chanche to load either the model with 
+        L2 regularization or the other model
+        Args:
+            name, name given to the model
+            custom_obj, dictionary that contains custom_objects
+                       
+        Returns:
+            the Keras RPN model
+    """
     model = load_model(name, custom_objects = custom_obj)
     for layer in model.layers:
         if (layer.name == 'block5_conv1' or layer.name == 'block5_conv2' 
@@ -52,3 +68,8 @@ def get_model_for_tuning(name, custom_obj):
     print(len(model.trainable_weights))
     print(len(model.non_trainable_weights))  
     return model
+  
+ """ It might be implemented a function to select the stage 
+     or use a user input function in main to select between training 
+     and fine_tuning and call the appropriate model"""
+        
