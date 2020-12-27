88e66c42406aaa528a8cf9a4c22b9d9984640f89
ekardnam <ekarndam@autistici.org>
2020-12-27 16:20:56 +0000

    Fix dataset preprocessing

--- head_rpn/dataset.py
+++ head_rpn/dataset.py
@@ -43,21 +43,28 @@ def load_kaggle_annotations(labels_path, boxes_path):
 
 def _map_object_node_to_bbox(object_node):
     bbox_node = object_node.find('bndbox')
-    xmin = int(bbox_node.find('xmin').text)
-    xmax = int(bbox_node.find('xmax').text)
-    ymin = int(bbox_node.find('ymin').text)
-    ymax = int(bbox_node.find('ymax').text)
+    xmin = int(float(bbox_node.find('xmin').text))
+    xmax = int(float(bbox_node.find('xmax').text))
+    ymin = int(float(bbox_node.find('ymin').text))
+    ymax = int(float(bbox_node.find('ymax').text))
     return [xmin, ymin, xmax, ymax]
 
 def annotation_from_xml_file(basepath, filename):
     filepath = os.path.join(basepath, filename)
-    xml_tree = ET.parse(filepath)
-    root = xml_tree.getroot()
-    filename = f'{os.path.splitext(filename)[0]}.jpeg'
-    size = root.find('size')
-    width = int(size.find('width').text)
-    height = int(size.find('height').text)
-    gt_boxes = [_map_object_node_to_bbox(object_node) for object_node in root.findall('object')]
+    try:
+        xml_tree = ET.parse(filepath)
+        root = xml_tree.getroot()
+        filename = f'{os.path.splitext(filename)[0]}.jpeg'
+        size = root.find('size')
+        width = int(size.find('width').text)
+        height = int(size.find('height').text)
+        gt_boxes = [
+                    _map_object_node_to_bbox(object_node)
+                    for object_node in filter(lambda object: object.find('bndbox') is not None, root.findall('object'))
+                   ]
+    except Exception as e:
+        print(f'Exception raised when processing file {filename}')
+        raise e
     return {
         'image_filename': filename,
         'image_width': width,
@@ -68,7 +75,7 @@ def annotation_from_xml_file(basepath, filename):
 
 def load_xml_annotations(annotations_folder):
     """
-        Loads the scut head dataset for head detection
+        Loads a XML head dataset for head detection
         Args:
             anotations_folder, the path to the folder containing annotations to load
         Returns:
--- scripts/create_tfrecords.py
+++ scripts/create_tfrecords.py
@@ -27,30 +27,38 @@ def _int64_feature(value):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Creates TFRecord files from a given Kaggle dataset')
-    parser.add_argument('labels', type=str, help='Path to the labels.csv file')
-    parser.add_argument('boxes', type=str, help='Path to the boxes.csv file')
-    parser.add_argument('scut_annotations_folder', type=str, help='The folder containing SCUT annotations')
-    parser.add_argument('kaggle_basepath', type=str, help='The base path for the Kaggle images')
-    parser.add_argument('scut_basepath', type=str, help='The base path for SCUT images')
+    parser.add_argument('xml_annotations_folder', type=str, help='The folder containing XML annotations')
+    parser.add_argument('xml_basepath', type=str, help='The base path for XML images')
+    parser.add_argument('--labels', type=str, help='Path to the labels.csv file')
+    parser.add_argument('--boxes', type=str, help='Path to the boxes.csv file')
+    parser.add_argument('--kagglebasepath', type=str, help='The base path for the Kaggle images')
     parser.add_argument('--out', type=str, default='out/data-{}.tfrecord', help='Format string for output TFRecords')
     parser.add_argument('--img-count', type=int, default=TFRECORD_IMG_COUNT, help='The number of images in each TFRecord file')
 
     args = parser.parse_args()
 
-    kaggle_annotations = load_kaggle_annotations(args.labels, args.boxes)
-    scut_annotations = load_xml_annotations(args.scut_annotations_folder)
+    kaggle_argument_is_not_none = [x is not None for x in [args.labels, args.boxes, args.kagglebasepath]]
+    if any(kaggle_argument_is_not_none):
+        if all(kaggle_argument_is_not_none):
+            kaggle_annotations = load_kaggle_annotations(args.labels, args.boxes)
+        else:
+            print('[-] If any Kaggle dataset argument is given than all of them should be used')
+            exit(0)
+    else:
+        kaggle_annotations = []
 
+    xml_annotations = load_xml_annotations(args.xml_annotations_folder)
     annotations = aggregate_annotations(
-                                        [kaggle_annotations, scut_annotations],
-                                        [args.kaggle_basepath, args.scut_basepath]
+                                        [kaggle_annotations, xml_annotations],
+                                        [args.kagglebasepath, args.xml_basepath]
                                        )
-    random.shuffle(annotations)                                   
+    random.shuffle(annotations)
 
     annotation_count = len(annotations)
 
-    for i in range(ceil(annotation_count / TFRECORD_IMG_COUNT)):
+    for i in range(ceil(annotation_count / args.img_count)):
         filename = args.out.format(i)
-        annotations_to_process = annotations[i * TFRECORD_IMG_COUNT : (i + 1) * TFRECORD_IMG_COUNT]
+        annotations_to_process = annotations[i * args.img_count : (i + 1) * args.img_count]
         with tf.python.python_io.TFRecordWriter(args.out.format(i)) as tfwriter:
             for annotation in annotations_to_process:
                 image = tf.io.read_file(annotation["image_filename"])
